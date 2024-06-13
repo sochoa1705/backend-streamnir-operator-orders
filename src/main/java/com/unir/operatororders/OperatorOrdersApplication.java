7f060cefa78739ac2b971d04eaeb9ee804cb2b6a
Sebastian <sebastianalejan.ochoa303@comunidadunir.net>
2024-06-12 22:53:20 -0500

    init project

---
+++ src/main/java/com/unir/operatororders/OperatorOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.unir.operatororders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperatorOrdersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OperatorOrdersApplication.class, args);
+    }
+
+}
